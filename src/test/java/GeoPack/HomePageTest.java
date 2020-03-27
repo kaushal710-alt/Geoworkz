d5df8f158b844bd6e3172b5ac11e7c3b21816a5d
kaushal710-alt <kaushalk7100@gmail.com>
2020-03-27 22:09:03 +0530

    15 days work

--- src/test/java/GeoPack/HomePageTest.java
+++ src/test/java/GeoPack/HomePageTest.java
@@ -21,5 +21,13 @@ public class HomePageTest extends BaseTest {
 	    home.getHomeLinkPresence();
 	    System.out.println("Git practice");
 	    System.out.println("Git practice1");
+	    System.out.println("Git practic2");
+	    System.out.println("Git practice3");
+	    System.out.println("Git practice4");
+	    System.out.println("Git practice5");
+	    System.out.println("Git practice6");
+	    System.out.println("Git practice7");
+	    System.out.println("Git practice8");
+	    System.out.println("Git practice9");
 	}
 }
