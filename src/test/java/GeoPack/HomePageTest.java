6d86f868e6911191eb7ab2e2e4a15a4dc92be4a2
kaushal710-alt <kaushalk7100@gmail.com>
2020-04-01 23:58:36 +0530

    final

--- src/test/java/GeoPack/HomePageTest.java
+++ src/test/java/GeoPack/HomePageTest.java
@@ -20,12 +20,6 @@ public class HomePageTest extends BaseTest {
 	{
 		HomePage home = new HomePage(driver);
 	    home.getHomeLinkPresence();
-<<<<<<< HEAD
-=======
-	   System.out.println("Started Integration automatically");
-		System.out.println("again commited changes123");
-	   
->>>>>>> 808e3f8df262662f366e751ddcc897d715043fc1
 	}
     
     @Test
